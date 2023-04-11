8d4ad32202abd77c57d12ca2d3ba7e12c7c8d5c0
Litchy_is_He <72008587+lithu-lithish@users.noreply.github.com>
2023-04-11 15:32:43 +0530

    Update settings.py

--- foodie/settings.py
+++ foodie/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+        'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,7 +54,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'foodie.urls'
 
 TEMPLATES = [
